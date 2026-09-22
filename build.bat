7c2724afe56d816180cbc46e8a5b96e093544629
yuanhao <lance_83@163.com>
2026-09-22 09:11:26 +0800

    修复了打包脚本的编码问题

--- build.bat
+++ build.bat
@@ -2,14 +2,14 @@
 chcp 65001 >nul
 cd /d "%~dp0"
 
-echo [1/2] 安装依赖...
+echo [1/2] Installing dependencies...
 python -m pip install -r requirements.txt pyinstaller
 if errorlevel 1 exit /b 1
 
-echo [2/2] 打包...
+echo [2/2] Building exe...
 python -m PyInstaller --noconfirm --onefile --windowed --name FloatingTextReader --icon icon.ico main.py
 if errorlevel 1 exit /b 1
 
 echo.
-echo 打包完成：dist\FloatingTextReader.exe
+echo Done: dist\FloatingTextReader.exe
 pause
